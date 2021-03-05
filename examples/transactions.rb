06b6fbac52fa57c9743ea9d8e89dac8e0dff18ae
Jeff Yip <jeffyip@users.noreply.github.com>
2021-03-05 15:41:25 -0800

    cleanup

--- examples/transactions.rb
+++ examples/transactions.rb
@@ -5,10 +5,10 @@ client = Berbix::Client.new(
   client_secret: ENV['BERBIX_DEMO_CLIENT_SECRET'],
 )
 
-tokens = client.create_transaction(customer_uid: '123', template_key: 'tpk_NEItvzFxRRSVFsAF4ZlR2u0sohqBm2Cg')
+tokens = client.create_transaction(customer_uid: 'ADD_UID_HERE', template_key: 'tpk_ADD_TEMPLATE_KEY_HERE')
 
 pp tokens
 
-hosted_transaction = client.create_hosted_transaction(customer_uid: '123', template_key: 'tpk_NEItvzFxRRSVFsAF4ZlR2u0sohqBm2Cg')
+hosted_transaction = client.create_hosted_transaction(customer_uid: 'ADD_UID_HERE', template_key: 'tpk_ADD_TEMPLATE_KEY_HERE', :hosted_options => {:completion_email => "ADD_EMAIL_HERE"})
 
 pp hosted_transaction.hosted_url
